45c82f14e0f7d202eeb7efd4f724db3d52327887
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-27 21:25:52 +0530

    chore(core): add bootstrap and angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+//router
 import { routes } from './app.routes';
+//third-party
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync()]
 };
