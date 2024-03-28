a25cddb741b2e3a4f26276215882c321c9304e5b
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-29 02:09:45 +0530

    feat(country-listing): add new component

---
+++ src/app/pages/country-listing/country-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-listing.component.html',
+  styleUrl: './country-listing.component.scss'
+})
+export class CountryListingComponent {
+
+}
