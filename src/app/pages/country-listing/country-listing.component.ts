c7cbdeadb6ef66421acf67aceed5106b8cb19f4e
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-29 20:22:51 +0530

    feat(country-listing): get the country data

--- src/app/pages/country-listing/country-listing.component.ts
+++ src/app/pages/country-listing/country-listing.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, JsonPipe } from '@angular/common';
+//service
+import { CountryListingService } from './country-listing.service';
+//model
+import { CountryListing } from './country-listing.model';
+//third-party
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-country-listing',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    CommonModule,
+    JsonPipe
+  ],
   templateUrl: './country-listing.component.html',
   styleUrl: './country-listing.component.scss'
 })
-export class CountryListingComponent {
+export class CountryListingComponent implements OnInit {
+  countryList: CountryListing[] = [];
+  selectedValue: string = 'population'
+  region: any = [];
+  selectedRegion: string;
 
+  constructor(private countryListingService: CountryListingService) {
+
+  }
+
+  ngOnInit() {
+    this.getCountryList();
+  }
+  
+  async getCountryList(): Promise<void> {
+    try {
+      const response = await this.countryListingService.getCountries().toPromise(); 
+      this.countryList = response;
+      console.log(this.countryList); 
+      this.region = [...new Set(this.countryList.map(data => data.region))];
+      console.log(this.region);
+      
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 }
