fff3b3c8f6ef82c2098ed9537cd8ed9364c4daaf
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-29 13:45:22 +0530

    feat(country-listing): add logic to get country list

--- src/app/pages/country-listing/country-listing.service.ts
+++ src/app/pages/country-listing/country-listing.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CountryListing } from './country-listing.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryListingService {
+  private baseUrl: string = 'https://restcountries.com/v3.1/';
+  private fields: string = '?fields=name,capital,population,area,flags,borders,region,independent,unMember,subregion,languages,currencies,continents,capital'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getCountries(): Observable<CountryListing[]> {
+    const urlWithParam = `${this.baseUrl}all/${this.fields}`
+    return this.http.get<CountryListing[]>(urlWithParam);
+  }
+
+  getCountriesByName(country: string): Observable<CountryListing> {
+    const urlWithParam = `${this.baseUrl}name/${country}`;
+    return this.http.get<CountryListing>(urlWithParam);
+  }
 }
