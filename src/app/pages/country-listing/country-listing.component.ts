230b5cc27ee56116d9397e382e1f679d84802589
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-29 22:14:39 +0530

    feat(country-listing): implement sort pipe

--- src/app/pages/country-listing/country-listing.component.ts
+++ src/app/pages/country-listing/country-listing.component.ts
@@ -1,27 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule, JsonPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 //service
 import { CountryListingService } from './country-listing.service';
 //model
 import { CountryListing } from './country-listing.model';
+//pipe
+import { SortPipe } from '../../shared/sort.pipe';
 //third-party
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-country-listing',
   standalone: true,
   imports: [
+    CommonModule,
+    FormsModule,
     MatInputModule,
     MatIconModule,
     MatSelectModule,
     MatFormFieldModule,
-    FormsModule,
-    CommonModule,
-    JsonPipe
+    MatCheckboxModule,
+    SortPipe
   ],
   templateUrl: './country-listing.component.html',
   styleUrl: './country-listing.component.scss'
@@ -39,15 +43,13 @@ export class CountryListingComponent implements OnInit {
   ngOnInit() {
     this.getCountryList();
   }
-  
+
   async getCountryList(): Promise<void> {
     try {
-      const response = await this.countryListingService.getCountries().toPromise(); 
+      const response = await this.countryListingService.getCountries().toPromise();
       this.countryList = response;
-      console.log(this.countryList); 
       this.region = [...new Set(this.countryList.map(data => data.region))];
-      console.log(this.region);
-      
+      this.region.sort();
     } catch (error) {
       console.error(error.message);
     }
