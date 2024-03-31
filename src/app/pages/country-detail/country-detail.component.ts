dad5959444dfbc1e3899b5740bbebbf00771d967
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-04-01 00:58:07 +0530

    feat(country-detail): get country details

--- src/app/pages/country-detail/country-detail.component.ts
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 //service
 import { CountryListingService } from '../country-listing/country-listing.service';
+//model
+import { CountryListing } from '../country-listing/country-listing.model';
 
 @Component({
   selector: 'app-country-detail',
@@ -12,15 +14,27 @@ import { CountryListingService } from '../country-listing/country-listing.servic
 })
 export class CountryDetailComponent implements OnInit {
   countryName: string;
+  countryDetail: CountryListing;
   constructor(
     private activatedRoute: ActivatedRoute,
     private countryListingService: CountryListingService
-    ) {}
+  ) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((param) => {
       this.countryName = param['name']
-    });  
+    });
+    this.getCountryDetail();
   }
 
+  getCountryDetail() {
+    this.countryListingService.getCountriesByName(this.countryName).subscribe({
+      next: (response: CountryListing) => {
+        this.countryDetail = response;
+      },
+      error: (error) => {
+        console.log(error.message);
+      }
+    });
+  }
 }
