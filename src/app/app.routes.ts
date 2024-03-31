6585e06a0e412e827169aa18ac4296dc4b3d07c9
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-31 16:32:58 +0530

    feat(home): add router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CountryListingComponent } from './pages/country-listing/country-listing.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/world-rank', pathMatch: 'full' },
-  { path: 'world-rank', component: HomeComponent}
+  { path: 'world-rank', component: HomeComponent ,children: [
+    { path: '', component: CountryListingComponent }
+  ]},
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 //component
 import { CountryListingComponent } from '../country-listing/country-listing.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CountryListingComponent],
+  imports: [CountryListingComponent, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
