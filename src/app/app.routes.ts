e59ed20541ef5561deeffb510733da278472992f
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-04-01 00:52:41 +0530

    (country-detail): add new component and add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CountryListingComponent } from './pages/country-listing/country-listing.component';
+import { CountryDetailComponent } from './pages/country-detail/country-detail.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/world-rank', pathMatch: 'full' },
   { path: 'world-rank', component: HomeComponent ,children: [
-    { path: '', component: CountryListingComponent }
+    { path: '', component: CountryListingComponent },
+    { path: ':name', component: CountryDetailComponent }
   ]},
 ];
---
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-detail.component.html',
+  styleUrl: './country-detail.component.scss'
+})
+export class CountryDetailComponent  {
+
+}
