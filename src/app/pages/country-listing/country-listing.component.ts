b4aa3e288863efb0266489366f428d5695ead976
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-04-05 14:10:00 +0530

    feat(country-listing): add search logic

--- src/app/pages/country-listing/country-listing.component.ts
+++ src/app/pages/country-listing/country-listing.component.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 //service
 import { CountryListingService } from './country-listing.service';
 //model
 import { CountryListing } from './country-listing.model';
 //pipe
 import { SortPipe } from '../../shared/sort.pipe';
-//third-party
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { SearchPipe } from '../../shared/search.pipe';
 
 @Component({
   selector: 'app-country-listing',
@@ -20,25 +16,20 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   imports: [
     CommonModule,
     FormsModule,
-    MatInputModule,
-    MatIconModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatCheckboxModule,
-    SortPipe
+    RouterLink,
+    SortPipe,
+    SearchPipe
   ],
   templateUrl: './country-listing.component.html',
-  styleUrl: './country-listing.component.scss'
+  styleUrls: ['./country-listing.component.scss']
 })
 export class CountryListingComponent implements OnInit {
   countryList: CountryListing[] = [];
-  selectedValue: string = 'population'
+  selectedValue: string = 'population';
   region: any = [];
-  selectedRegion: string;
+  searchValue: string;
 
-  constructor(private countryListingService: CountryListingService) {
-
-  }
+  constructor(private countryListingService: CountryListingService) {}
 
   ngOnInit() {
     this.getCountryList();
