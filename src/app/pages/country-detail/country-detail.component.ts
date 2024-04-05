3a92d354e19098a965e1250af50c99f8bcab625d
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-04-05 14:05:47 +0530

    feat(country-detail): get countries by code

--- src/app/pages/country-detail/country-detail.component.ts
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 //service
 import { CountryListingService } from '../country-listing/country-listing.service';
 //model
@@ -8,13 +9,14 @@ import { CountryListing } from '../country-listing/country-listing.model';
 @Component({
   selector: 'app-country-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './country-detail.component.html',
   styleUrl: './country-detail.component.scss'
 })
 export class CountryDetailComponent implements OnInit {
   countryName: string;
   countryDetail: CountryListing;
+  countryCodeListing: any[];
   constructor(
     private activatedRoute: ActivatedRoute,
     private countryListingService: CountryListingService
@@ -31,10 +33,23 @@ export class CountryDetailComponent implements OnInit {
     this.countryListingService.getCountriesByName(this.countryName).subscribe({
       next: (response: CountryListing) => {
         this.countryDetail = response;
+        this.countryListingService.getCountriesbyCode(this.countryDetail[0].borders).subscribe({
+          next: (response: string[]) => {
+            this.countryCodeListing = response;
+          },
+          error: (error) => {
+            console.log(error.message);
+          }
+        });
       },
       error: (error) => {
         console.log(error.message);
       }
     });
   }
+
+  getCurrencyName(currency: any): string {
+    return currency?.name || 'Unknown';
+  }
+
 }
